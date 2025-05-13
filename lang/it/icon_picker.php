8a30bfaaf419585b56fb901f840d8e21d075b063
marco76tv <marco.sottana@gmail.com>
2025-05-13 08:45:25 +0200

    .

--- lang/it/icon_picker.php
+++ lang/it/icon_picker.php
@@ -1,14 +1,19 @@
 <?php
 
-declare(strict_types=1);
-
-return [
-    'fields' => [
-        'newstate' => [
-            'label' => 'newstate',
-        ],
-        'pack' => [
-            'label' => 'pack',
-        ],
-    ],
-];
+return array (
+  'fields' => 
+  array (
+    'newstate' => 
+    array (
+      'label' => 'newstate',
+    ),
+    'pack' => 
+    array (
+      'label' => 'pack',
+    ),
+    'icon' => 
+    array (
+      'label' => 'icon',
+    ),
+  ),
+);
