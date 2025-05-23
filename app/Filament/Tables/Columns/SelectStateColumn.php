efb04cc664e00ae563d520a26048de1fccab60fe
marco76tv <marco.sottana@gmail.com>
2025-05-23 13:05:57 +0200

    .

--- app/Filament/Tables/Columns/SelectStateColumn.php
+++ app/Filament/Tables/Columns/SelectStateColumn.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Modules\UI\Filament\Tables\Columns;
 
 use Exception;
+use Illuminate\Support\Arr;
 use Spatie\ModelStates\State;
 use Modules\SaluteOra\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Tables\Columns\SelectColumn;
 use Modules\SaluteOra\States\User\UserState;
@@ -20,20 +23,34 @@ class SelectStateColumn extends SelectColumn
       //  $this->selectablePlaceholder(false);
         $this->options(function (Model $record ,$state): array {
             $name=$this->getName();
-            
-            
-            try{
-                $states=$record->getAttribute($name)->transitionableStates();
-            }catch(Exception $e){
-                $states=$states=$record->getStatesFor($name)->toArray();;
-            }
-            $states[]=$state::$name;
+            if($state==null){
 
-            
-            return array_combine($states, $states);
+                $states=Arr::wrap($record->getDefaultStateFor($name));
+                return array_combine($states, $states);
+            }
+            try{
+                //$states=$record->getAttribute($name)->transitionableStates();
+                $states=$state->transitionableStates();
+            }catch(Exception $e){
+                $states=$record->getStatesFor($name)->toArray();;
+            }
+            $states=[$state::$name, ...$states];
+            $states=array_combine($states, $states);
+            //dddx(['state'=>$state, 'state1'=>$record->getAttribute($name),'record'=>$record]);
+
+            return $states;
         });
-       
+
+
+        $this->beforeStateUpdated(function (Model $record, $state) {
+            $message='';
+            $record->state->transitionTo($state,$message);
+        });
+
+
     }
 
-   
+
+
+
 }
