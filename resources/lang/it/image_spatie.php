541b81d9746cfa57f3c43ae6c3c08c5e716be547
Malebestia <malebestia@yahoo.it>
2025-06-24 16:26:32 +0200

    .

---
+++ resources/lang/it/image_spatie.php
@@ -0,0 +1,28 @@
+<?php
+
+return array (
+  'fields' => 
+  array (
+    'img_uuid' => 
+    array (
+      'label' => 'img_uuid',
+      'placeholder' => 'img_uuid',
+      'helper_text' => 'img_uuid',
+      'description' => 'img_uuid',
+    ),
+    'image' => 
+    array (
+      'label' => 'image',
+      'placeholder' => 'image',
+      'helper_text' => 'image',
+      'description' => 'image',
+    ),
+    'caption' => 
+    array (
+      'label' => 'caption',
+      'placeholder' => 'caption',
+      'helper_text' => 'caption',
+      'description' => 'caption',
+    ),
+  ),
+);
