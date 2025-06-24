b6f36664f63322ebfdb0f09c7d10f5be3093ae4b
marco76tv <marco.sottana@gmail.com>
2025-06-24 17:42:02 +0200

    .

---
+++ resources/lang/it/icon_picker.php
@@ -0,0 +1,14 @@
+<?php
+
+return array (
+  'fields' => 
+  array (
+    'pack' => 
+    array (
+      'label' => 'pack',
+      'placeholder' => 'pack',
+      'helper_text' => 'pack',
+      'description' => 'pack',
+    ),
+  ),
+);
