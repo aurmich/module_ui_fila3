b5508d7e7db944b741720184ceeb900a2acad554
marco76tv <marco.sottana@gmail.com>
2025-09-03 21:27:26 +0200

    .

--- resources/views/components/dropdown/separator.blade.php
+++
@@ -1,1 +0,0 @@
-<div class="border-t border-gray-200 dark:border-gray-700 my-1"></div> 
--- resources/views/components/filament/dropdown/separator.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- 
-    Componente separatore per dropdown di Filament 
-    Questo componente aggiunge una linea di separazione visiva all'interno dei dropdown.
---}}
-<div class="border-t border-gray-200 dark:border-gray-700 my-1"></div>
