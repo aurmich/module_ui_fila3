68dec25afa4d7826a0312a9c17b7e6faff15e27b
marco76tv <marco.sottana@gmail.com>
2025-09-10 11:01:22 +0200

    .

--- app/View/Components/DarkModeSwitcher.php
+++ app/View/Components/DarkModeSwitcher.php
@@ -37,31 +37,17 @@ class DarkModeSwitcher extends Component
     public function render(): View
     {
         // Verifica se il widget può essere visualizzato
-<<<<<<< HEAD
-        if (!DarkModeSwitcherWidget::canView()) {
-=======
         if (! DarkModeSwitcherWidget::canView()) {
->>>>>>> 1485e9d (.)
             /** @var view-string $view */
             $view = 'ui::components.empty';
             return view($view);
         }
 
-<<<<<<< HEAD
-        // Ottiene i dati dal widget usando il metodo pubblico getPlaceholderData()
-        $viewData = $this->widget->getPlaceholderData();
-
-        /** @var array<string, mixed> $viewData */
-        /** @var view-string $view */
-        $view = 'ui::filament.widgets.dark-mode-switcher';
-        return view($view, $viewData);
-=======
         // Ottiene i dati pubblici dal widget
         $viewData = $this->widget->getPublicViewData();
 
         /** @var view-string $view */
         $view = 'ui::components.dark-mode-switcher';
         return \view($view, $viewData);
->>>>>>> 1485e9d (.)
     }
 }
